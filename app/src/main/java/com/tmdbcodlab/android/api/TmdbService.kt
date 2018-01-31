0858492f1cd2d5dfa2c967fc5d24f2bf71766245
Moshe <pnemonic@gmail.com>
2018-01-31 18:14:39 +0200

    https://developers.themoviedb.org/3/movies/get-now-playing

--- app/src/main/java/com/tmdbcodlab/android/api/TmdbService.kt
+++ app/src/main/java/com/tmdbcodlab/android/api/TmdbService.kt
@@ -1,9 +1,20 @@
 package com.tmdbcodlab.android.api
 
+import com.tmdbcodlab.android.model.Movie
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 /**
  * Created by ronelg on 12/19/17.
  */
 interface TmdbService {
+
+    @GET("/movie/now_playing")
+    fun getMoviesNowPlaying(@Query("api_key") apiKey: String,
+                            @Query("language") language: String?,
+                            @Query("page") page: Int? = 1,
+                            @Query("region") region: String?): Single<List<Movie>>
 
 }
--- app/src/main/java/com/tmdbcodlab/android/model/Movie.kt
+++ app/src/main/java/com/tmdbcodlab/android/model/Movie.kt
@@ -1,6 +1,21 @@
 package com.tmdbcodlab.android.model
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by ronelg on 12/19/17.
  */
-data class Movie(val id: String)
+data class Movie(@SerializedName("id") val id: Long,
+                 @SerializedName("poster_path") val posterPath: String?,
+                 @SerializedName("adult") val adult: Boolean,
+                 @SerializedName("overview") val overview: String,
+                 @SerializedName("release_date") val releaseDate: String,
+                 @SerializedName("genre_ids") val genreIds: Array<Long>,
+                 @SerializedName("original_title") val original_title: String,
+                 @SerializedName("original_language") val originalLanguage: String,
+                 @SerializedName("title") val title: String,
+                 @SerializedName("backdrop_path") val backdropPath: String?,
+                 @SerializedName("popularity") val popularity: Float,
+                 @SerializedName("vote_count") val voteCount: Integer,
+                 @SerializedName("video") val video: Boolean,
+                 @SerializedName("vote_average") val voteAverage: Float)
