c5253d98f22dd6af79a28b5c6258f2bfa2772958
Moshe <pnemonic@gmail.com>
2018-02-07 12:28:02 +0200

    dagger

--- app/src/main/java/com/tmdbcodlab/android/MyApplication.kt
+++ app/src/main/java/com/tmdbcodlab/android/MyApplication.kt
@@ -1,12 +1,20 @@
 package com.tmdbcodlab.android
 
 import android.app.Application
+import com.tmdbcodlab.android.inject.components.ApplicationComponent
+import com.tmdbcodlab.android.inject.components.DaggerApplicationComponent
 import timber.log.Timber
 
 class MyApplication : Application() {
 
+    companion object {
+        lateinit var appComponent: ApplicationComponent
+    }
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        appComponent = DaggerApplicationComponent.create()
+        appComponent.inject(this)
     }
 }
