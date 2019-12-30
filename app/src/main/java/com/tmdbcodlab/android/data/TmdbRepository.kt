b117b752a94bd9a2a37ec6304827f11eda3c3df5
M. Waisberg <pnemonic@gmail.com>
2019-12-30 17:09:14 +0200

    Observable.concat

--- app/src/main/java/com/tmdbcodlab/android/data/TmdbRepository.kt
+++ app/src/main/java/com/tmdbcodlab/android/data/TmdbRepository.kt
@@ -21,22 +21,26 @@ class TmdbRepository(private val localRepository: TmdbLocalDataSource,
     private val cache = LruCache<String, Observable<*>>(100)
 
     override fun getMoviesNowPlaying(): Observable<List<Movie>> {
-        val result = localRepository.getMoviesNowPlaying()
-                .switchIfEmpty(remoteRepository.getMoviesNowPlaying())
+        val result = Observable.concat(
+            localRepository.getMoviesNowPlaying(),
+            remoteRepository.getMoviesNowPlaying()
+        )
 
         return cacheObservable(CACHE_PREFIX_MOVIE_NOW_PLAYING, result)
     }
 
     override fun getMovieDetails(movieId: Long): Observable<MovieDetails> {
-        val result = localRepository.getMovieDetails(movieId)
-                .switchIfEmpty(remoteRepository.getMovieDetails(movieId))
+        val result = Observable.concat(
+            localRepository.getMovieDetails(movieId),
+            remoteRepository.getMovieDetails(movieId)
+        )
 
         return cacheObservable(CACHE_PREFIX_MOVIE_DETAILS + movieId, result)
     }
 
     @SuppressWarnings("unchecked")
     private fun <T> cacheObservable(key: String, observable: Observable<T>): Observable<T> {
-        var cachedObservable: Observable<T>? = cache.get(key) as Observable<T>?
+        val cachedObservable: Observable<T>? = cache.get(key) as Observable<T>?
         if (cachedObservable != null) {
             return cachedObservable
         }
